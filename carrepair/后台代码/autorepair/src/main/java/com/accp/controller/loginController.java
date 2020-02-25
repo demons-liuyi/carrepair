8a080fe6cd07b0ae10abd8cf406d5718d24f6a40
liuyi <15364237386@sina.cn>
2020-02-25 11:38:38 +0800

    刘艺于2/25上传

--- carrepair/后台代码/autorepair/src/main/java/com/accp/controller/loginController.java
+++ carrepair/后台代码/autorepair/src/main/java/com/accp/controller/loginController.java
@@ -1,5 +1,7 @@
 package com.accp.controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -17,12 +19,13 @@ public class loginController {
 	
 	@ResponseBody
 	@RequestMapping("/selectStaffByAccountAndPassword")	 
-	public String selectStaffByAccountAndPassword(String account,String password) {	 
+	public String selectStaffByAccountAndPassword(String account,String password,HttpSession session) {	 
 		Staff staff=ss.selectStaffByAccountAndPassword(account,password);
 		System.out.println(staff.getStaffno());	 
 		if(staff.getStaffno()==null||staff.getStaffno()==""||staff.getStaffno().equals("")||staff.getStaffno()==" ") {
 			return "0";
 		}
+		session.setAttribute("staff", staff);
 	 return staff.getStaffno();
 		 
 	}
